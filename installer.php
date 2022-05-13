cf6d8b824874f1a474dd918437c57f9148a6d5d9
Toby Payne <75158953+tpguy825@users.noreply.github.com>
2022-05-13 17:42:18 +0100

    Update installer.php

--- installer.php
+++ installer.php
@@ -1,10 +1,15 @@
 <?php
 
+$downloadedamount = json_decode(file_get_contents("https://api.countapi.xyz/update/pmmpinstaller.cf/5b01a783-a15f-4aa3-a534-36cc79988fe3?amount=1"),true)['value'];
+
+$version = file_get_contents("https://raw.githubusercontent.com/tpguy825/PMMPInstaller/main/version.txt");
+
+echo "Welcome to PMMPInstaller by tpguy825!\n\nYou are using version $version.\n\n"; 
 $api = json_decode(file_get_contents("https://update.pmmp.io/api"),true);
 $url = $api['download_url'];
 $lessurl = "https://github.com/pmmp/PocketMine-MP/releases/download/".$api['base_version']."/";
 
-echo "\nDownloading latest version (".$api['base_version'].") for MC version (".$api['mcpe_version'].") ";
+echo "\nDownloading latest PMMP version (".$api['base_version'].") for MC version (".$api['mcpe_version'].") ";
 
 copy($url, "PocketMine-MP.phar");
 copy($lessurl."start.cmd", "start.cmd");
