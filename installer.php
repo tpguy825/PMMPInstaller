133a651fee188c86e86e8a615747f4cbacffbe96
Toby Payne <75158953+tpguy825@users.noreply.github.com>
2022-05-13 18:19:46 +0100

    Update installer.php

--- installer.php
+++ installer.php
@@ -1,10 +1,14 @@
 <?php
 
-$downloadedamount = json_decode(file_get_contents("https://api.countapi.xyz/update/pmmpinstaller.cf/5b01a783-a15f-4aa3-a534-36cc79988fe3?amount=1"),true)['value'];
+$dltimes = json_decode(file_get_contents("https://api.countapi.xyz/update/pmmpinstaller.cf/5b01a783-a15f-4aa3-a534-36cc79988fe3?amount=1"),true)['value'];
+
+$dltimesend =(int) mb_substr($dltimes, strlen($dltimes)-1);
+ 
+if($dltimesend === 1) { $dltimes = $dltimes."st"; } elseif($dltimesend === 2) { $dltimes = $dltimes."nd"; } elseif($dltimesend === 3) { $dltimes = $dltimes."rd" } else { $dltimes = $dltimes."th" }
 
 $version = file_get_contents("https://raw.githubusercontent.com/tpguy825/PMMPInstaller/main/version.txt");
 
-echo "Welcome to PMMPInstaller by tpguy825!\n\nYou are using version $version.\n\n"; 
+echo "Welcome to PMMPInstaller by tpguy825!\n\nYou are using version $version and was the $dltimes person to download this.\n\n"; 
 $api = json_decode(file_get_contents("https://update.pmmp.io/api"),true);
 $url = $api['download_url'];
 $lessurl = "https://github.com/pmmp/PocketMine-MP/releases/download/".$api['base_version']."/";
@@ -16,5 +20,5 @@ copy($lessurl."start.cmd", "start.cmd");
 copy($lessurl."start.ps1", "start.ps1");
 copy($lessurl."start.sh", "start.sh");
 
-echo "Success! You can now delete 'installer.php'";
+echo "Success! You can now run 'start.cmd', 'start.ps1' or 'start.sh'";
 unlink('installer.php');
