36bfb80395ee90a5ef0dc7239cd37941d2b1563f
tpguy825 <75158953+tpguy825@users.noreply.github.com>
2022-05-16 19:10:16 +0100

    update installer.php

--- installer.php
+++ installer.php
@@ -5,27 +5,43 @@
 if(isset($argv[1]) && $argv[1] === "dev") {
  $dltimes = "null";
  
-} else {
-$dltimes = json_decode(file_get_contents("https://api.countapi.xyz/update/pmmpinstaller.cf/5b01a783-a15f-4aa3-a534-36cc79988fe3?amount=1"),true)['value'];
+} elseif(isset($argv[1]) && $argv[1] === "startfile") {
+    $api = json_decode(file_get_contents("https://update.pmmp.io/api"),true);
+    $url = $api['download_url'];
+    $lessurl = "https://raw.githubusercontent.com/tpguy825/PMMPInstaller/main/";
+    $version = file_get_contents("https://raw.githubusercontent.com/tpguy825/PMMPInstaller/main/version.txt");
+    $oldversion = file_get_contents("version.txt");
 
-$dltimesend =(int) mb_substr($dltimes, strlen($dltimes)-1);
- 
-if($dltimesend === 1) { $dltimes = $dltimes."st"; } elseif($dltimesend === 2) { $dltimes = $dltimes."nd"; } elseif($dltimesend === 3) { $dltimes = $dltimes."rd" } else { $dltimes = $dltimes."th" }
+    if($version > $oldversion) {
+        copy($url, "PocketMine-MP.phar");
+        copy($lessurl."start.cmd", "start.cmd");
+        copy($lessurl."start.ps1", "start.ps1");
+        copy($lessurl."start.sh", "start.sh");
+        echo "[PMMPInstaller] Updated from version $oldversion to $version";
+        unlink('installer.php');
+        exit();
+    }
 }
 
-$version = file_get_contents("https://raw.githubusercontent.com/tpguy825/PMMPInstaller/main/version.txt");
+    $dltimes = json_decode(file_get_contents("https://api.countapi.xyz/update/pmmpinstaller.cf/5b01a783-a15f-4aa3-a534-36cc79988fe3?amount=1"),true)['value'];
+    
+    $dltimesend =(int) mb_substr($dltimes, strlen($dltimes)-1);
+    
+    if($dltimesend === 1) { $dltimes = $dltimes."st"; } elseif($dltimesend === 2) { $dltimes = $dltimes."nd"; } elseif($dltimesend === 3) { $dltimes = $dltimes."rd"; } else { $dltimes = $dltimes."th"; }
 
-echo "Welcome to PMMPInstaller by tpguy825!\n\nYou are using version $version and was the $dltimes person to download this.\n\n"; 
-$api = json_decode(file_get_contents("https://update.pmmp.io/api"),true);
-$url = $api['download_url'];
-$lessurl = "https://raw.githubusercontent.com/tpguy825/PMMPInstaller/main/";
+    $version = file_get_contents("https://raw.githubusercontent.com/tpguy825/PMMPInstaller/main/version.txt");
 
-echo "\nDownloading latest PMMP version (".$api['base_version'].") for MC version (".$api['mcpe_version'].") ";
+    echo "Welcome to PMMPInstaller by tpguy825!\n\nYou are using version $version and was the $dltimes person to download this.\n\n"; 
+    $api = json_decode(file_get_contents("https://update.pmmp.io/api"),true);
+    $url = $api['download_url'];
+    $lessurl = "https://raw.githubusercontent.com/tpguy825/PMMPInstaller/main/";
 
-copy($url, "PocketMine-MP.phar");
-copy($lessurl."start.cmd", "start.cmd");
-copy($lessurl."start.ps1", "start.ps1");
-copy($lessurl."start.sh", "start.sh");
+    echo "\nDownloading latest PMMP version (".$api['base_version'].") for MC version (".$api['mcpe_version'].") ";
 
-echo "Success! You can now run 'start.cmd', 'start.ps1' or 'start.sh'";
-unlink('installer.php');
+    copy($url, "PocketMine-MP.phar");
+    copy($lessurl."start.cmd", "start.cmd");
+    copy($lessurl."start.ps1", "start.ps1");
+    copy($lessurl."start.sh", "start.sh");
+
+    echo "Success! You can now run 'start.cmd', 'start.ps1' or 'start.sh'";
+    unlink('installer.php');
