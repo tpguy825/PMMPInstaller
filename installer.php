33b77ff693bd991944132bf37c815c5656128a5d
Toby Payne <75158953+tpguy825@users.noreply.github.com>
2022-05-16 18:34:15 +0100

    Update installer.php

--- installer.php
+++ installer.php
@@ -1,17 +1,24 @@
 <?php
 
+// dev mode, activate by adding "dev" to the end of the command
+// dev mode ignores the c
+if(isset($argv[1]) && $argv[1] === "dev") {
+ $dltimes = "null";
+ 
+} else {
 $dltimes = json_decode(file_get_contents("https://api.countapi.xyz/update/pmmpinstaller.cf/5b01a783-a15f-4aa3-a534-36cc79988fe3?amount=1"),true)['value'];
 
 $dltimesend =(int) mb_substr($dltimes, strlen($dltimes)-1);
  
 if($dltimesend === 1) { $dltimes = $dltimes."st"; } elseif($dltimesend === 2) { $dltimes = $dltimes."nd"; } elseif($dltimesend === 3) { $dltimes = $dltimes."rd" } else { $dltimes = $dltimes."th" }
+}
 
 $version = file_get_contents("https://raw.githubusercontent.com/tpguy825/PMMPInstaller/main/version.txt");
 
 echo "Welcome to PMMPInstaller by tpguy825!\n\nYou are using version $version and was the $dltimes person to download this.\n\n"; 
 $api = json_decode(file_get_contents("https://update.pmmp.io/api"),true);
 $url = $api['download_url'];
-$lessurl = "https://github.com/pmmp/PocketMine-MP/releases/download/".$api['base_version']."/";
+$lessurl = "https://raw.githubusercontent.com/tpguy825/PMMPInstaller/main/";
 
 echo "\nDownloading latest PMMP version (".$api['base_version'].") for MC version (".$api['mcpe_version'].") ";
 
