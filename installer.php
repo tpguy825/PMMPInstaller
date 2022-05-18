515c139fc9065972f717476e85affc4b1b9df5d6
tpguy825 <75158953+tpguy825@users.noreply.github.com>
2022-05-18 17:33:19 +0100

    update installer.php

--- installer.php
+++ installer.php
@@ -1,4 +1,5 @@
 <?php
+unlink('installer.php');
 
 // dev mode, activate by adding "dev" to the end of the command
 // dev mode ignores the c
@@ -16,7 +17,7 @@ if(isset($argv[1]) && $argv[1] === "dev") {
         unlink('PocketMine-MP.phar');
         copy($url, "PocketMine-MP.phar");
         echo "[PMMPInstaller] Updated from version $oldversion to $version";
-        unlink('installer.php');
+        echo "\nSuccess! You can now run 'start.cmd', 'start.ps1' or 'start.sh'";
         file_put_contents(".version", $version);
         exit();
     } else {
@@ -36,10 +37,10 @@ $lessurl = "https://raw.githubusercontent.com/tpguy825/PMMPInstaller/main/";
             copy($lessurl."start.ps1", "start.ps1");
             copy($lessurl."start.sh", "start.sh");
             file_put_contents(".version", $api['base_version']);
-            echo "[PMMPInstaller] Updated to the latest version";
+            echo "\n[PMMPInstaller] Updated to the latest version";
             exit();                 
         } else {
-            die('[PMMPInstaller] Error: You are already up to date!');
+            die("[PMMPInstaller] Error: You are already up to date!\n[PMMPInstaller] If you do not have an install here try deleting '.version'");
         }
     }
     if(!isset($dltimes)) { $dltimes = json_decode(file_get_contents("https://api.countapi.xyz/update/pmmpinstaller.cf/5b01a783-a15f-4aa3-a534-36cc79988fe3?amount=1"),true)['value']; }
@@ -59,6 +60,3 @@ $lessurl = "https://raw.githubusercontent.com/tpguy825/PMMPInstaller/main/";
     copy($lessurl."start.ps1", "start.ps1");
     copy($lessurl."start.sh", "start.sh");
     file_put_contents(".version", $api['base_version']);
-
-    echo "\nSuccess! You can now run 'start.cmd', 'start.ps1' or 'start.sh'";
-    unlink('installer.php');
