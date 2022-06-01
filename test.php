c7ae7e90089ea119647639ab5b09451488844eb0
Toby Payne <75158953+tpguy825@users.noreply.github.com>
2022-06-01 13:45:23 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,18 +1,21 @@
 <?php
 
 class GetVersionInfo {
-    publuc function 
+    private $vi
+    public function __construct($vi) {
+        $this->vi = $vi;
+    }
+
     public function getversion() {
-        return VersionInfo::BASE_VERSION;
+        return $this->vi->BASE_VERSION;
     }
 }
 
 try {
     $phar = new Phar('./PocketMine-MP.phar');
     $phar->extractTo('./pmmp-phar', 'src/VersionInfo.php', true);
-    $phar->extractTo('./pmmp-phar', 'src/Server.php', true);
-    require('./pmmp-phar/src/Server.php');
-    $version = (new Server(new DynamicClassLoader, new AttachableThreadedLogger, "", ""))->getApiVersion();
+    require('./pmmp-phar/src/VersionInfo.php');
+    $version = (new GetVersionInfo(new VersionInfo))->getversion();
     echo $version;
 } catch (PharException $e) {
     echo $e;
